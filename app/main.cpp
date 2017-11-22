f9d4a1e849657fecd9d74153b1feb4aae2628c22
tqtiimy <5281060@qq.com>
2017-11-22 22:38:00 +0800

    test

--- app/main.cpp
+++ app/main.cpp
@@ -17,6 +17,7 @@ int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
 
+	//ÉèÖÃ×Ö·û¼¯
 	QTextCodec *gbk = QTextCodec::codecForName("GBK");
 	QTextCodec::setCodecForTr(gbk);
 	QTextCodec::setCodecForLocale(gbk);
